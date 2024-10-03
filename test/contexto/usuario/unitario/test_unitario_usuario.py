bccebb6a8cd95b5911ca2b4241262b306cbf1d5b
Bruno Estrela <brunosiqest2@gmail.com>
2024-10-03 12:25:35 -0300

    test(test/unit): add teste para atualizar entidade usuario

--- test/contexto/usuario/unitario/test_unitario_usuario.py
+++ test/contexto/usuario/unitario/test_unitario_usuario.py
@@ -13,3 +13,22 @@ def test_criacao_de_usuario():
     assert usuario.email == "email@email.com"
     assert usuario.senha == "123456"
     assert usuario.nivel_de_acesso == "USUARIO"
+
+
+def test_atualizacao_de_usuario():
+
+    usuario = Usuario.criar(
+        nome="João", email="email@email", senha="123456", nivel_de_acesso="USUARIO"
+    )
+
+    usuario.atualizar(
+        nome="João da Silva",
+        email="novo@email",
+        senha="654321",
+        nivel_de_acesso="ADMIN",
+    )
+
+    assert usuario.nome == "João da Silva"
+    assert usuario.email == "novo@email"
+    assert usuario.senha == "654321"
+    assert usuario.nivel_de_acesso == "ADMIN"
