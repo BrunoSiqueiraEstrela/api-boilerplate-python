b1f2b29a5f711deb6da1fc08efc87f7242ea1536
Bruno Estrela <brunosiqest2@gmail.com>
2024-12-04 02:59:53 -0300

    feat(database): add config funcao para conectar com banco

--- libs/database/config.py
+++ libs/database/config.py
@@ -1,7 +1,18 @@
 from typing import Final
 from sqlalchemy.orm import registry
-from sqlalchemy import MetaData
-
+from sqlalchemy import Engine, MetaData, create_engine
+from sqlalchemy.orm.session import sessionmaker
+from libs.variaveis.gerenciador_de_env import ENVS
 
 registro_dos_orms: Final = registry()
 metadata = MetaData()
+
+
+def conectar():
+
+    engine: Engine = create_engine(
+        ENVS.DB_STRING_CONNECTION, isolation_level="AUTOCOMMIT"
+    )
+    _session = sessionmaker(bind=engine, expire_on_commit=False)
+
+    return _session()
