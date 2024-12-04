cb55361ad0ce3cee3ab4979f0740dfa33c2b8957
Bruno Estrela <brunosiqest2@gmail.com>
2024-12-04 03:00:39 -0300

    chore(database): add retorno na funcao

--- libs/database/config.py
+++ libs/database/config.py
@@ -1,14 +1,14 @@
 from typing import Final
 from sqlalchemy.orm import registry
 from sqlalchemy import Engine, MetaData, create_engine
-from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.orm.session import Session, sessionmaker
 from libs.variaveis.gerenciador_de_env import ENVS
 
 registro_dos_orms: Final = registry()
 metadata = MetaData()
 
 
-def conectar():
+def conectar() -> Session:
 
     engine: Engine = create_engine(
         ENVS.DB_STRING_CONNECTION, isolation_level="AUTOCOMMIT"
