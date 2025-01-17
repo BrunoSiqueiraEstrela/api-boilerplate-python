e49bd9c8831cfe99298a5925a0c569399700b838
Bruno_Estrela <brunosiqest2@gmail.com>
2025-01-17 13:30:34 -0300

    feature: rodar app provisorio

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import os
+import uvicorn
+
 if __name__ == "__main__":
-    print("Hello World!")
+    HOST = os.environ.get("HOST", "127.0.0.1")
+    # PORT: int = os.environ.get("PORT", 8080)
+
+    uvicorn.run("servidor.config:app", host=HOST, port=8888, reload=True)
