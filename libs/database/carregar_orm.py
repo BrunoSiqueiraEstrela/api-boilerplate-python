49c6bf5969a4565d56ad988c7a95cd1a30c34cd0
Bruno_Estrela <brunosiqest2@gmail.com>
2025-01-20 18:23:47 -0300

    fix: fix importacao

--- libs/database/carregar_orm.py
+++ libs/database/carregar_orm.py
@@ -1,3 +1,3 @@
 def carregar_tabelas():
-    from contexto.usuario.repositorios.orm.usuario import tabela_usuario
-    from contexto.tarefa.repositorios.orm.tarefa import tabela_tarefa
+    from contexto.usuario.repositorio.orm.usuario import tabela_usuario  # noqa
+    from contexto.tarefa.repositorio.orm.tarefa import tabela_tarefa  # noqa
